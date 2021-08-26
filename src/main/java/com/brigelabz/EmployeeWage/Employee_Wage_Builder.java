f8fdc519eb599800e24580d2ae4b0ca424211ea8
sangharsh23 <sangharsh355@gmail.com>
2021-08-26 13:21:09 +0530

    Added: Part Time Employee Wage

--- src/main/java/com/brigelabz/EmployeeWage/Employee_Wage_Builder.java
+++ src/main/java/com/brigelabz/EmployeeWage/Employee_Wage_Builder.java
@@ -4,19 +4,24 @@ public class Employee_Wage_Builder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int FULL_TIME_IS = 1;
+        int PART_TIME_IS = 2;
         int EMP_WAGE_PER_HOUR = 20;
         int empHrs;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == FULL_TIME_IS) {
-            System.out.println("Employee is present");
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        System.out.println("empCheck : " +empCheck);
+        if (empCheck == FULL_TIME_IS){
+            System.out.println("Employee is Full Time present");
             empHrs = 8;
         }
-        else{
+        else if(PART_TIME_IS == empCheck) {
+            System.out.println("Employee is Part Time Present");
+            empHrs = 4;
+        }
+        else {
             System.out.println("Employee is Absent");
             empHrs = 0;
-    }
-
-    int daily_Wage = EMP_WAGE_PER_HOUR * empHrs;
-        System.out.println("daily_Wage : "+daily_Wage);
+        }
+    int empWage = EMP_WAGE_PER_HOUR * empHrs;
+        System.out.println("empWage : "+empWage);
   }
 }
