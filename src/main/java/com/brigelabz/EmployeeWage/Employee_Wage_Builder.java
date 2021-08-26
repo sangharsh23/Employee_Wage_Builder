920627cb52b37407aa3c09475975b4ab0b570700
sangharsh23 <sangharsh355@gmail.com>
2021-08-26 15:24:25 +0530

    Added: Max 100 Hour empWage for 20day

--- src/main/java/com/brigelabz/EmployeeWage/Employee_Wage_Builder.java
+++ src/main/java/com/brigelabz/EmployeeWage/Employee_Wage_Builder.java
@@ -5,16 +5,18 @@ public class Employee_Wage_Builder {
     public static final int PART_TIME_IS = 2;
     public static final int EMP_WAGE_PER_HOUR = 20;
     public static final int NO_OF_WORK_DAY = 20;
-
+    public static final int MAX_MONTH_WORK_HRS = 100;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int empHrs = 0;
-        int day = 0;
+        int totalWorkday = 0;
+        int totalEmpHrs = 0;
         int totalWage = 0;
-        for (day = 1; day <= 20; day++) {
+        while ( totalEmpHrs <= MAX_MONTH_WORK_HRS && totalWorkday < NO_OF_WORK_DAY) {
+            totalWorkday++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             System.out.println("empCheck : " + empCheck);
-            System.out.println("day "+ day);
+            System.out.println("Day : " +totalWorkday);
             switch (empCheck) {
                 case FULL_TIME_IS:
                     System.out.println("Employee is Full Time present");
@@ -28,11 +30,10 @@ public class Employee_Wage_Builder {
                     System.out.println("Employee is Absent");
                     empHrs = 0;
             }
-
-            int empWage = EMP_WAGE_PER_HOUR * empHrs;
-            System.out.println("empWage : " + empWage);
-            totalWage += empWage;
-            System.out.println("total Wage: " + totalWage);
+            totalEmpHrs += empHrs;
+            System.out.println("totalWorkday" + " Emp Hrs :" + empHrs);
+        }
+            int totalEmpWage = EMP_WAGE_PER_HOUR * totalEmpHrs;
+            System.out.println("total Emp Wage: " + totalEmpWage);
         }
     }
-}
