9ec468915ea3c3fa3dbdc56aad29ad61ff1768a7
sangharsh23 <sangharsh355@gmail.com>
2021-08-26 12:58:01 +0530

    Added: Employee Daily Wage

--- src/main/java/com/brigelabz/EmployeeWage/Employee_Wage_Builder.java
+++ src/main/java/com/brigelabz/EmployeeWage/Employee_Wage_Builder.java
@@ -4,11 +4,19 @@ public class Employee_Wage_Builder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int FULL_TIME_IS = 1;
-        double empCheck = Math.floor(Math.random() * 10 ) % 2;
-        if (empCheck == FULL_TIME_IS)
+        int EMP_WAGE_PER_HOUR = 20;
+        int empHrs;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == FULL_TIME_IS) {
             System.out.println("Employee is present");
-        else
+            empHrs = 8;
+        }
+        else{
             System.out.println("Employee is Absent");
+            empHrs = 0;
     }
 
+    int daily_Wage = EMP_WAGE_PER_HOUR * empHrs;
+        System.out.println("daily_Wage : "+daily_Wage);
+  }
 }
