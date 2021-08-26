fb1c1fa0a5b52981ddde3b6376e7374a09c3cd84
sangharsh23 <sangharsh355@gmail.com>
2021-08-26 12:24:51 +0530

    Added: Employee is Present or Absent

--- src/main/java/com/brigelabz/EmployeeWage/Employee_Wage_Builder.java
+++ src/main/java/com/brigelabz/EmployeeWage/Employee_Wage_Builder.java
@@ -3,6 +3,12 @@ package com.brigelabz.EmployeeWage;
 public class Employee_Wage_Builder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        int FULL_TIME_IS = 1;
+        double empCheck = Math.floor(Math.random() * 10 ) % 2;
+        if (empCheck == FULL_TIME_IS)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is Absent");
     }
 
 }
